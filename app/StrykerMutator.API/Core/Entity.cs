5632a0368a98afce02a4fb08dd0ed971b5fe6437
Jairon Alves Lima <jairon.alves.lima@gmail.com>
2024-03-23 19:12:01 -0300

    feat: setup project

---
+++ app/StrykerMutator.API/Core/Entity.cs
@@ -0,0 +1,14 @@
+﻿namespace StrykerMutator.API.Core
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public int NumberOne { get; set; }
+        public int NumberTwo { get; set; }
+                
+        public int Sum()
+        {
+            return NumberOne + NumberTwo;
+        }
+    }
+}
